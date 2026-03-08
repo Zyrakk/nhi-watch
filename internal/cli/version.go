21b4a5473e6f2a0b0c2fa407b20d89a1d8842606
Hivemind <hivemind@zyrak.cloud>
2026-03-08 11:41:02 +0000

    feat: add build metadata to version output

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -2,16 +2,28 @@ package cli
 
 import (
 	"fmt"
-	"runtime"
+	"io"
 
 	"github.com/spf13/cobra"
 )
+
+func versionInfoString() string {
+	return fmt.Sprintf("version=%s commit=%s buildDate=%s", Version, Commit, BuildDate)
+}
+
+func refreshRootVersion() {
+	rootCmd.Version = versionInfoString()
+}
+
+func printVersionInfo(w io.Writer) {
+	fmt.Fprintln(w, versionInfoString())
+}
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the NHI-Watch version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("nhi-watch %s (%s/%s)\n", Version, runtime.GOOS, runtime.GOARCH)
+		printVersionInfo(cmd.OutOrStdout())
 	},
 }
 
